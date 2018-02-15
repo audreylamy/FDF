29c5c61777ca883108244e636acc4b2fc092b1c7
Audrey LAMY <alamy@e1r10p12.42.fr>
2018-02-15 10:28:15 +0100

    fdf.h fixed + commentaires

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: alamy <alamy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/13 11:49:43 by alamy             #+#    #+#             */
-/*   Updated: 2018/02/15 10:15:03 by alamy            ###   ########.fr       */
+/*   Updated: 2018/02/15 10:27:22 by alamy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -133,6 +133,9 @@ typedef struct	s_env
 
 int				main(int argc, char **argv);
 
+/*
+**CHECKS
+*/
 int				ft_checks(char **argv, t_env *tmp);
 int				ft_check_name_file(char *str);
 int				ft_empty_file(char *str);
@@ -142,6 +145,9 @@ int				ft_isxdigit(char *str);
 int				ft_open_failed(int fd);
 int				ft_close_failed(int fd);
 
+/*
+**PARSING
+*/
 int				ft_nb_line(char **argv);
 int				ft_getnbr(char *str);
 int				ft_lenght(char **str);
@@ -150,14 +156,23 @@ void			ft_print_tab(t_env *tmp);
 void			ft_parse_map(char **argv, t_env *tmp);
 void			ft_get_coord(char *line, t_env *tmp);
 
+/*
+**IMAGES
+*/
 void			ft_create_image(t_env *tmp);
 void			ft_create_new_image(t_env *tmp, int keycode);
 void			fill_pixel(t_env *tmp, int x, int y, int color);
 
+/*
+**MAP TRANSFORMATIONS
+*/
 void			ft_transform_map(t_env *tmp, int keycode);
 void			ft_if_error_map(t_env *tmp);
 void			ft_reset_event(t_env *tmp);
 
+/*
+**BRESENHAM
+*/
 void			ft_draw_line_horiz(t_env *tmp);
 void			ft_draw_line_vertical(t_env *tmp);
 void			print_horiz_line(t_env *tmp, int i, int j);
@@ -166,6 +181,9 @@ void			ft_bresenham(t_data *data, t_env *tmp);
 void			ft_bre_1(t_algob *b, t_env *tmp, t_data *data, int color);
 void			ft_bre_2(t_algob *b, t_env *tmp, t_data *data, int color);
 
+/*
+**EVENTS
+*/
 void			ft_redraw_image(t_env *tmp, int keycode);
 int				my_key_funct(int keycode, t_env *tmp);
 int				my_mouse_funct(int button, int x, int y, t_env *tmp);
@@ -175,8 +193,14 @@ void			event_zoom_projection(int keycode, t_env *tmp);
 void			event_color(int keycode, t_env *tmp);
 void			event_clic(int button, t_env *tmp);
 
+/*
+**COLOR
+*/
 t_color			ft_color_converter(t_env *tmp, int hexvalue);
 
+/*
+**CREATE STRING
+*/
 void			ft_create_string(t_env *tmp);
 
 typedef struct	s_vector4
@@ -192,6 +216,18 @@ typedef struct	s_matrix
 	float	m[4][4];
 }				t_matrix4;
 
+/*
+**TRANSFORMATIONS
+*/
+t_vector4		ft_two_transformations(t_data *data, t_env *tmp, int keycode);
+t_vector4		ft_create_transformation(t_data *data, t_env *tmp);
+t_vector4		ft_create_rot_zoom(t_vector4 vec_h, t_vector4 result, t_env *t);
+t_vector4		ft_reset_transformation(t_data *data, t_env *tmp);
+t_vector4		ft_reset_rot_zoom(t_vector4 vec_h, t_vector4 result, t_env *t);
+
+/*
+**MATRIX
+*/
 t_matrix4		matrix_identity(void);
 t_matrix4		matrix_translation(t_vector4 vector);
 t_matrix4		matrix_homothetie(t_vector4 homo);
@@ -200,11 +236,6 @@ t_matrix4		matrix_rotation_y(float alpha);
 t_matrix4		matrix_rotation_z(float alpha);
 t_matrix4		matrix_proj(float angle, float ratio, float near, float far);
 t_vector4		create_vector4(int x, int y, int z, int w);
-t_vector4		ft_two_transformations(t_data *data, t_env *tmp, int keycode);
-t_vector4		ft_create_transformation(t_data *data, t_env *tmp);
-t_vector4		ft_create_rot_zoom(t_vector4 vec_h, t_vector4 result, t_env *t);
-t_vector4		ft_reset_transformation(t_data *data, t_env *tmp);
-t_vector4		ft_reset_rot_zoom(t_vector4 vec_h, t_vector4 result, t_env *t);
 t_vector4		ft_cal_translation(t_vector4 vector2, t_matrix4 m_trans);
 t_vector4		ft_cal_rotation_x(t_vector4 vector, t_matrix4 m_rotation_x);
 t_vector4		ft_cal_rotation_y(t_vector4 vector, t_matrix4 m_rotation_y);
